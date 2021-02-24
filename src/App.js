8b5892ebdbf9a7422b953ddf8ac5bda788600a36
Tatyna Kubatova <tatynakubatova74@gmail.com>
2021-02-24 14:41:12 +0600

    Added several components

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 // import React from 'react';
 import HelloWorld from "./components/HelloWorld";
 import ByeWorld from "./components/ByeWorld";
+import Chunga from "./components/Chunga";
+import Hochu from "./components/Hochu";
 
 function App() {
   return (
     <div className="App">
       <HelloWorld />
-      <HelloWorld />
-      <HelloWorld />
-      <HelloWorld />
       <ByeWorld />
+      <Chunga />
+      <Hochu v='more' />
+      <Hochu v='kosmos' />
     </div>
   );
 }
---
+++ src/components/Chunga.js
@@ -0,0 +1,5 @@
+function Chunga(){
+    return <h1>Changa</h1>;
+}
+
+export default Chunga;
