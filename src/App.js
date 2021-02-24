94cd15f8b655d4ca30782751a40a5c90fa6bc258
Tatyna Kubatova <tatynakubatova74@gmail.com>
2021-02-24 14:31:33 +0600

    Added Bye world component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,15 @@
+// import React from 'react';
+import HelloWorld from "./components/HelloWorld";
+import ByeWorld from "./components/ByeWorld";
+
 function App() {
   return (
     <div className="App">
+      <HelloWorld />
+      <HelloWorld />
+      <HelloWorld />
+      <HelloWorld />
+      <ByeWorld />
     </div>
   );
 }
