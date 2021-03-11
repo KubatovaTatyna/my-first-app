5b189c5440485fee018ccd20448485a999d83dc3
Tatyna Kubatova <tatynakubatova74@gmail.com>
2021-03-11 18:40:18 +0600

    Changed app js content

--- src/App.js
+++ src/App.js
@@ -1,53 +1,43 @@
-import Switch from "./components/Switch";
-import Name from "./components/Name";
-import DoubleInput from "./components/DoubleInput";
-import List from "./components/List";
-import Counter from "./components/Counter";
-import InputLength from "./components/InputLength";
-import Color from "./components/Color";
-import InputRange from "./components/InputRange";
-import Dollar from "./components/Dollar";
-import Temp from "./components/Temp";
-import FontSize from "./components/FontSize";
-import InputColor from "./components/InputColor";
-import ColorSliders from "./components/ColorSliders";
-import RotateInput from "./components/RotateInput";
-import MirroredInput from "./components/MirroredInput";
-
+import "./App.css"
+import Person from "./components/Person/Person";
+import Button from "./components/Button/Button";
+import SolidButton from "./components/SolidButton/SolidButton";
+import BackgroundButton from "./components/BackgroundButton/BackgroundButton";
+import DashedButton from "./components/DashedButton/DashedButton";
+import DottedButton from "./components/DottedButton/DottedButton";
+import BorderButton from "./components/BorderButton/BorderButotn";
+import PaddingButton from "./components/PaddingButton/PaddingButton";
+import BorderSolid from "./components/BorderSolid/BorderSolid";
+import CapitalButton from "./components/CapitalButton/CapitalButton";
+import DottedBorderButton from "./components/DottedBorderButton/DottedBorderButton";
+import PersonList from "./components/PersonList/PersonList";
 
 function App() {
-
+  const persons = [
+    { name: "Elon Baike", age: 47, hobby: "Ice skating, Eating donuts, Dancing under the moon" },
+    { name: "Bill Baike", age: 50, hobby: "Saving the world" }
+  ];
   return (
-    <div>
-      <Switch />
-      <h1>Example 1</h1>
-      <Name />
-      <hr></hr>
-      <h1>Example 2</h1>
-      <DoubleInput />
-      <hr></hr>
-      <h1>Example 3</h1>
-      <List />
-      <h1>Example 4</h1>
-      <Counter />
-      <h1>Example 5</h1>
-      <InputLength value= "test" />
-      <h1>Example 6</h1>
-      <Color value="#45335" />
-      <h1>Example 7</h1>
-      <InputRange min={0} max={10} />
-      <h1>Example 8</h1>
-      <Dollar />
-      <h1>Example 9</h1>
-      <Temp />
-      <FontSize />
-      <InputColor />
-      <ColorSliders />
-      <br></br>
-      <RotateInput />
-      <hr></hr>
-      <MirroredInput />
+    <div className="App">
+      <Person name="Elon " age={47}>
+        Ice skating, Eating donuts, Dancing under the moon
+      </Person>
+      <Person name="Bill " age={50}>
+        Saving the world
+      </Person>
+      <span className="highlight">Hello</span>
+      <Button>Click</Button>
+      <SolidButton>Style 1</SolidButton>
+      <BackgroundButton>Style 2</BackgroundButton>
+      <DashedButton>Style 3</DashedButton>
+      <DottedButton>Style 4</DottedButton>
+      <BorderButton>Style 5</BorderButton>
+      <PaddingButton>Style 6</PaddingButton>
+      <BorderSolid>Style 7</BorderSolid>
+      <CapitalButton>Style 8</CapitalButton>
+      <DottedBorderButton>Style 9</DottedBorderButton>
+      <PersonList persons={persons} />
     </div>
-  )
+  );
 }
 export default App;
